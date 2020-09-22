c73e4229b2b3e879fde744eee25b5ed42be15d8c
Fede Lemaire <fede.lemaire@gmail.com>
2020-09-22 11:59:33 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const App = () => (
+  <>
+    <h1>Welcome to React Parcel Micro App!</h1>
+    <p>Hard to get more minimal than this React app jajajaja.</p>
+  </>
+);
+
+export default App;
