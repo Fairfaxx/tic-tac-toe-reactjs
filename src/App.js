92e6573173eac962e11051760431c9c83d8f526a
Fede Lemaire <fede.lemaire@gmail.com>
2020-09-22 15:32:15 -0300

    added sass and styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Board from "./components/Board";
 
+import './components/styles/root.scss';
+
 const App = () => (
-  <div>
+  <div className='app'>
     <h1>TIC TAC TOE</h1>
     <Board />
   </div>
